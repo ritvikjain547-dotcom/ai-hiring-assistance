d54a727e89582f23120a7963fa2930f69aa7c8dc
ritvikjain <ritvikjain547@gmail.com>
2026-06-18 15:16:31 +0530

    fix: allow account linking for Google Auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "credentials",
